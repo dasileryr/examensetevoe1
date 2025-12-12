57e03d7082a1b43fe818cd9a9327b743d9f59062
Denis Zadvornyy <denis281107@yandex.ru>
2025-12-12 10:20:01 +0300

    up4

--- Models/DownloadItem.cs
+++ Models/DownloadItem.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace FileDownloader.Models
 {
@@ -13,15 +15,59 @@ namespace FileDownloader.Models
         Cancelled
     }
 
-    public class DownloadItem
+    public class DownloadItem : INotifyPropertyChanged
     {
+        private long _totalBytes;
+        private long _downloadedBytes;
+        private DownloadStatus _status;
+
         public int Id { get; set; }
         public string Url { get; set; } = string.Empty;
         public string SavePath { get; set; } = string.Empty;
         public string FileName { get; set; } = string.Empty;
-        public long TotalBytes { get; set; }
-        public long DownloadedBytes { get; set; }
-        public DownloadStatus Status { get; set; }
+        
+        public long TotalBytes
+        {
+            get => _totalBytes;
+            set
+            {
+                if (_totalBytes != value)
+                {
+                    _totalBytes = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Progress));
+                }
+            }
+        }
+        
+        public long DownloadedBytes
+        {
+            get => _downloadedBytes;
+            set
+            {
+                if (_downloadedBytes != value)
+                {
+                    _downloadedBytes = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(Progress));
+                }
+            }
+        }
+        
+        public DownloadStatus Status
+        {
+            get => _status;
+            set
+            {
+                if (_status != value)
+                {
+                    _status = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(StatusText));
+                }
+            }
+        }
+        
         public int ThreadCount { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? CompletedAt { get; set; }
@@ -29,6 +75,7 @@ namespace FileDownloader.Models
         public List<string> Tags { get; set; } = new List<string>();
 
         public double Progress => TotalBytes > 0 ? (double)DownloadedBytes / TotalBytes * 100 : 0;
+        
         public string StatusText => Status switch
         {
             DownloadStatus.Pending => "Ожидание",
@@ -39,6 +86,13 @@ namespace FileDownloader.Models
             DownloadStatus.Cancelled => "Отменено",
             _ => "Неизвестно"
         };
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
 
